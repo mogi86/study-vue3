71195113f3f3b6795086d1d185aa64ea8bfe1a8e
mogi86 <nobuyuki.mogi@gmail.com>
2022-05-07 23:45:10 +0900

    use env

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,7 +2,8 @@
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
+    const host = Cypress.env('host')
+    cy.visit(host + '/')
     cy.contains('h1', 'Welcome to Your Vue.js App')
   })
 })
